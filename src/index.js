c9998a34477cbb2bb35af5d4db432b49371a825b
muunji <twoji3019@naver.com>
2024-11-21 15:18:03 +0900

    readFileSync 추가

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer(function(request, response){
   if(request.method === "GET") {
     if(request.url === "/") {
-      response.writeHead(200,{"Content-type":"text/html;charset='utf-8'"})
-      response.write();
+      const pathUrl = path.join(__dirname,"../public/index.html");
+      const responseData = fs.readFileSync(pathUrl,'utf-8',function(err,data){
+        if(err){
+          console.error(err);
+        }
+        return data;
+      })
+      response.writeHead(200,"utf-8",{"Content-type":"text/html"})
+      response.write(responseData);
       response.end();
     }
   }
 })
 
 server.listen(3000,function(){
-  console.log("http://localhos:3000");
+  console.log("http://localhost:3000");
 })
